80237908322e7ed8e677ce9eef6503ee2f3f29f6
AimanFikri <irkifnamia@gmail.com>
2026-07-03 20:38:08 +0800

    setup initial streamlit project and database planning

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Mathematics Unit Analytics Webapp",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Mathematics Unit Analytics Webapp")
+
+st.write("Welcome to Version 1.0")
